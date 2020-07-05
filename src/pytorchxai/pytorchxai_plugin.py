ab42937ab04fc3a2f1eb6092c81c55f6004eb0fd
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-07-05 19:19:55 +0300

    add demo saliency map generator

--- src/pytorchxai/pytorchxai_plugin.py
+++ src/pytorchxai/pytorchxai_plugin.py
@@ -20,7 +20,7 @@ class PyTorchXAIPlugin(base_plugin.TBPlugin):
         plugin_directory_path_part = f"/data/plugin/{self.plugin_name}/"
         self._multiplexer = context.multiplexer
         self._offset_path = len(plugin_directory_path_part)
-        self._prefix_path = Path(__file__).parent / 'pytorchxai'
+        self._prefix_path = Path(__file__).parent / "pytorchxai"
 
     def is_active(self):
         """Returns whether there is relevant data for the plugin to process.
@@ -28,9 +28,7 @@ class PyTorchXAIPlugin(base_plugin.TBPlugin):
         When there are no runs with greeting data, TensorBoard will hide the
         plugin from the main navigation bar.
         """
-        return bool(
-            self._multiplexer.PluginRunToTagToContent(self.plugin_name)
-        )
+        return bool(self._multiplexer.PluginRunToTagToContent(self.plugin_name))
 
     def get_plugin_apps(self):
         return {
@@ -39,8 +37,7 @@ class PyTorchXAIPlugin(base_plugin.TBPlugin):
 
     def frontend_metadata(self):
         return base_plugin.FrontendMetadata(
-            es_module_path="/static/index.js",
-            tab_name="PyTorchXAI"
+            es_module_path="/static/index.js", tab_name="PyTorchXAI"
         )
 
     @wrappers.Request.application
@@ -56,6 +53,4 @@ class PyTorchXAIPlugin(base_plugin.TBPlugin):
         resource_absolute_path = str(self._prefix_path / resource_path)
         with open(resource_absolute_path, "rb") as read_file:
             mimetype = mimetypes.guess_type(resource_absolute_path)[0]
-            return http_util.Respond(
-                request, read_file.read(), content_type=mimetype
-            )
+            return http_util.Respond(request, read_file.read(), content_type=mimetype)
