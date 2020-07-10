58a0ec3cd24058fdfecda97e8d4623174b201b69
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-07-10 11:39:40 +0300

    smooth grad

--- src/pytorchxai/xai/gradient_smooth_grad.py
+++ src/pytorchxai/xai/gradient_smooth_grad.py
@@ -1,3 +1,11 @@
+"""
+SmoothGrad technique is adding some Gaussian noise to the original image and calculating gradients multiple times and averaging the results.
+It can augment other sensitivity techniques, such as: vanilla gradients, integrated gradients, guided backpropagation or GradCam.
+
+
+[1] D. Smilkov, N. Thorat, N. Kim, F. Viégas, M. Wattenberg. "SmoothGrad: removing noise by adding noise", 2017.
+"""
+
 import numpy as np
 import torch
 from torch.autograd import Variable
@@ -16,13 +24,14 @@ class SmoothGrad:
         self, prep_img, target_class, param_n, param_sigma_multiplier
     ):
         """
-            Generates smooth gradients of given Backprop type.
-            You can use this with both vanilla and guided backprop
+            Generates smooth gradients of given backprop type: vanilla or guided.
         Args:
-            prep_img (torch Variable): preprocessed image
+            prep_img (torch Variable): preprocessed image.
             target_class (int): target class of imagenet
-            param_n (int): Amount of images used to smooth gradient
-            param_sigma_multiplier (int): Sigma multiplier when calculating std of noise
+            param_n (int): Amount of images used to smooth gradient.
+            param_sigma_multiplier (int): Sigma multiplier when calculating std of noise.
+        Returns:
+            The gradients.
         """
         # Generate an empty image/matrix
         smooth_grad = np.zeros(prep_img.size()[1:])
@@ -47,7 +56,16 @@ class SmoothGrad:
         return smooth_grad
 
     def generate(self, orig_image, input_image, target_class):
-        param_n = 50
+        """
+            Generates and returns multiple sensitivy heatmaps, based on SmoothGrad technique.
+            Args:
+                orig_image: Original resized image.
+                input_image: Preprocessed input image.
+                target_class: Expected category.
+            Returns:
+                Colored and grayscale gradients for the SmoothGrad backpropagation.
+        """
+        param_n = 5
         param_sigma_multiplier = 4
         smooth_grad = self.generate_smooth_grad(
             input_image, target_class, param_n, param_sigma_multiplier
