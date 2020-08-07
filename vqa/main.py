fe28c66e1c89e485f2e9e424027c16f5c7ff7f7b
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-08-07 13:57:58 +0300

    add API over VQA

---
+++ vqa/main.py
@@ -0,0 +1,26 @@
+import sys
+
+import inference
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+sys.path.append("./MICCAI19-MedVQA")
+
+
+class Question(BaseModel):
+    image_b64: str
+    question: str
+
+
+app = FastAPI()
+
+VQA = inference.VQA()
+
+
+@app.post("/vqa")
+def vqa_query(q: Question):
+    try:
+        result = VQA.ask(q.question, q.image_b64)
+        return {"answer": result}
+    except BaseException as e:
+        return {"error": str(e)}
